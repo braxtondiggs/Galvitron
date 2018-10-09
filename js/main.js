fce591b5037daa3e2b89fb72a07f64375dd204b5
Braxton Diggs <braxtondiggs@gmail.com>
2018-10-09 18:14:22 -0400

    Add profile

--- js/main.js
+++ js/main.js
@@ -17,5 +17,20 @@
   });
   $('.title-input').typeahead({ source: ['VP of Communications', 'VP of Investments', 'VP of Marketing', 'VP of Operations', 'VP of Sales'] });
   $('.company-input').typeahead({ source: ['Bücker Flugzeugbau', 'Buick', 'BUM Equipment', 'Bushwick Daily', 'Bushwick Kitchen', 'Buw Consulting', 'BUX'] });
-  $('.skill-input').typeahead({ source: ['investment banking', 'investment planning'] });
+  $('.skill-input').typeahead({ source: ['investment banking', 'investment planning'], afterSelect:  function() {
+    $('.chips').append(`<li>${$(this.$element.get(0)).val()}<span class="delete">&times;</span></li>`);
+    $(this.$element.get(0)).val('');
+  }});
+  $('.chips').on('click', 'li', function() {
+    $(this).remove();
+  });
+  $('.user-image').on('click', () => $('input[type="file"]').trigger('click'));
+  $('#welcomeModal').modal('show');
+  $('.profile-view .action-menu').popover({
+    html: true,
+    placement: 'bottom',
+    content: function() {
+      return $($(this).attr("data-popover-content")).html();
+    }
+  }).popover('show');
 })();
